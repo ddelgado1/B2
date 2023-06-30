3afa765fa6781308485d43d1457f32db24458526
ddelgado1 <ddelgado@wbwood.com>
2023-06-29 21:01:11 -0400

    Initial ServerSide Commit.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+// const cors = require('cors');
+
+const app = express();
+
+// app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
